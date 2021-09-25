b28be03d26da861575d6b4cf5e7bbff2b8a2cbf0
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-09-24 19:47:10 -0700

    initial psuedo code for javascript logic written

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 /* Psuedo Code Steps
-1. Create global variables that link to html
-2.Create global variable for my javascript file
 
+1. Create global variables that link to html
+2. Create arrays for Questions, Answers & Correct answers  
+3. Creating and setting number values for my times variables
+4. Start game function
+5. Reset game function
+6. End game function
+7. Functions for setting and getting scores from local storage
+8. Functions to check for wrong answers and deduct time when wrong'
+9. Function to increase time when answer is right
+10.For loop that iterates through questions
 
 */
