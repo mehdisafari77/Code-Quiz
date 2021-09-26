e499a1fbfe826d4a435d56be5543252d73a3a8ee
Mehdi Safari <sa.mehdisafari@gmail.com>
2021-09-25 21:49:59 -0700

    add initial function for game start

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,8 @@
 2. Create arrays for Questions, Answers & Correct answers  
 3. Creating and setting number values for my times variables
 4. Start game function
-5. Reset game function
-6. End game function
+5. End game function
+6. Reset game function
 7. Functions for setting and getting scores from local storage
 8. Functions to check for wrong answers and deduct time when wrong'
 9. Function to increase time when answer is right
@@ -14,43 +14,62 @@
 */
 
 // HTML element selectors
- var startButton = document.querySelector(".start-button")
- var scoreButton = document.querySelector(".scoreboard")
+var startButton = document.querySelector(".start-button")
+var scoreButton = document.querySelector(".scoreboard")
 
- // Global reusable variables
- var score = 0;
- var currentQuestion = -1;
- var timeLeft = 0;
- var timer;
+// Global reusable variables
+var score = 0;
+var currentQuestion = -1;
+var timeLeft = 0;
+var timer;
 
- // Question Object Arrays
- var questions = [{
+// Question Object Arrays
+var questions = [{
     title: "Which of the following methods has the ability to combine two arrays and return one new array?",
     choices: ["map( )", "sort( )", "splice( )", "concat( )"],
     answer: "concat( )"
-  },
-  {
+    },
+{
+    title: "",
+    choices: [],
+    answer: ""
+    },
+{
     title: "",
     choices: [],
     answer: ""
   },
-  {
+{
     title: "",
     choices: [],
     answer: ""
   },
-  {
+{
     title: "",
     choices: [],
     answer: ""
   },
-  {
+{
     title: "",
     choices: [],
     answer: ""
-  },
-  {
-    title: "",
-    choices: [],
-    answer: ""
-  }]
+}]
+
+function startGame() {
+    timeLeft = 75;
+    document.querySelector("time-counter").innerHTML = time-counter;
+  
+    timer = setInterval(function() {
+        timeLeft--;
+        document.getElementById("time-counter").innerHTML = time-counter;
+  
+        // Run endgame function when timer equals 0
+        if (timeLeft <= 0) {
+            // clear game func position
+            (timer);
+            // end game func position; 
+        }
+    }, 1000);
+  
+   // question for loop function position;
+  }
